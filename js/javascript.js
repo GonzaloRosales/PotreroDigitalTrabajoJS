60203ee1644633b9a102edbefe249c419dc69a3e
Gonzalo Rosales <gonzalo.rosales992@gmail.com>
2022-07-09 16:06:09 -0300

    mi primer commit

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function cambiarfondo1() { 
+    document.body.style.background = "black";
+    
+}
+
+function copy() {alert("by Gonzalo Rosales")}
+
+function cartel() {alert("Potrero Digital")}
+
+alert("Practico 8. Funcionamiento de JS. Bienvenido!")
